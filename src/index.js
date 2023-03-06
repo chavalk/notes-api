3cb6ad7f658462c563a1fb08487cf8e7cde6e27e
chavalk <chavalk@hotmail.com>
2023-03-05 21:58:31 -0600

    Add code to read notes.json file when get request is submitted.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const fs = require('fs')
 const app = express();
 
 app.get('/notes', (req, res) => {
+    fs.readFile(__dirname + '/' + "notes.json", "utf-8", (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
 
+        res.send(data)
+    })
 })
 
 app.listen(3000, () => {
