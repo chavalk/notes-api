355000c7b41b98fa1bdbddc11b2abd048519dbd6
chavalk <chavalk@hotmail.com>
2023-03-05 21:24:55 -0600

    Add call to get notes.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/notes', (req, res) => {
+
+})
+
 app.listen(3000, () => {
     console.log("Server is up on port 3000")
 })
